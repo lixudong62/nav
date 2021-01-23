3d2590acb7511e202999930a6050704fb7cfbd8b
lixudong62 <47598463+lixudong62@users.noreply.github.com>
2021-01-24 00:21:25 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -3,7 +3,7 @@ import { IConfig } from './src/types'
 // 网站 LOGO 替换根目录下的 logo.png， 务必为 png 后缀
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/lixudong62/nav',
 
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
   hashMode: true,
